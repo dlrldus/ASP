5d7d1c3003444f5f662af701bf4c540d8378d80b
thkim0515 <thkim0515@naver.com>
2020-09-02 22:27:48 +0900

    테스트용 aspx

---
+++ Default.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="Site.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeaderContent" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+    <asp:Label runat="server" id ="MysqlTest" Text="Mysql_Test_label"></asp:Label><br />
+    <asp:Button runat="server" text="테스트버튼" ID ="Btntest" OnClick="Btntest_Click" />
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="FooterContent" Runat="Server">
+</asp:Content>
+
---
+++ Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Btntest_Click(object sender, EventArgs e)
+    {
+        MySqlConnection connection = new MySqlConnection("Server=ec2-13-124-56-141.ap-northeast-2.compute.amazonaws.com;Database=2020Project;Uid=root;Pwd=qwer1234;");
+        connection.Open();
+        string insertQuery = "INSERT INTO reply (nickname,comment,writedate,postnumber) values(2020,0902,1,59)";
+
+        MySqlCommand command = new MySqlCommand(insertQuery, connection);
+        command.ExecuteNonQuery();
+
+        connection.Close();
+    }
+}
+
